4221382f68d48e405715b0e9d8354c6eb9354cac
t-ryles <tajhakeem.ryles@gmail.com>
2023-08-15 21:34:05 -0500

    Implementing Insertions Sort tests.

--- src/test/kotlin/InsertionSortTest.kt
+++ src/test/kotlin/InsertionSortTest.kt
@@ -1,31 +1,31 @@
-import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase
 import org.junit.Test
 
-class BubbleSortTest {
+class InsertionSortTest{
     @Test
-    fun test1BubbleSort(){
+    fun test1InsertionSort(){
         val unsortedList = mutableListOf(64, 34, 25, 12, 22, 11, 90)
         val sortedList = BubbleSort().bubbleSort(unsortedList.toMutableList())
 
         val expectedList = listOf(11, 12, 22, 25, 34, 64, 90)
-        assertEquals(expectedList, sortedList)
+        TestCase.assertEquals(expectedList, sortedList)
     }
 
     @Test
-    fun test2BubbleSort(){
+    fun test2InsertionSort(){
         val unsortedList = mutableListOf(5, 2, 9, 1, 5, 6)
         val sortedList = BubbleSort().bubbleSort(unsortedList.toMutableList())
 
         val expectedList = listOf(1,2,5,5,6,9)
-        assertEquals(expectedList, sortedList)
+        TestCase.assertEquals(expectedList, sortedList)
     }
 
     @Test
-    fun test3BubbleSort(){
+    fun test3InsertionSort(){
         val unsortedList = mutableListOf(12, 45, 23, 7, 3, 56, 8, 2)
         val sortedList = BubbleSort().bubbleSort(unsortedList.toMutableList())
 
         val expectedList = listOf(2,3,7,8,12,23,45,56)
-        assertEquals(expectedList, sortedList)
+        TestCase.assertEquals(expectedList, sortedList)
     }
 }
